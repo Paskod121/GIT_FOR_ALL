fe72e05368068e0ce18b2b8a2a1b2c158886bf9c
Joseph-ADAMAH <adamahkouevijoseph@gmail.com>
2025-07-31 08:29:17 +0000

    Modifier par Joseph en fonction de l'exercice demandé

--- hello.py
+++ hello.py
@@ -1,26 +1,5 @@
 print("Hello")
 print("Faure")
-# Voici un petit programme de jeu de devinette
-# Ecrire par Joseph
 
-import random
-
-print("Bienvenu dans notre programme de devinette")
-#devine = random.randint(0,10)
-compteur = 0
-while True:
-    devine = random.randint(0, 10)
-    nbre = float(input("Veuillez entrer votre nombre : "))
-    if nbre == devine:
-        print("Excellent vous avez trouvez le nombre")
-        compteur += 1
-        break
-    elif nbre > devine:
-
-        print("Dommage ! Votre nombre  est trop grand ")
-        compteur += 1
-    elif nbre < devine :
-        print("Dommage ! Votre nombre est trop petit ")
-        compteur += 1
-
-print("Le nombre d'essai : ", compteur)
+print("Hello ")
+print("Venant de la part de Joseph")
